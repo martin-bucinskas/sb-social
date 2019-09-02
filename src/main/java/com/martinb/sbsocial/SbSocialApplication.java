7a8154402d89aedc5a5740bd6c0421aff4ec99ff
Martin Bucinskas <martin.bucinskas@enginegroup.com>
2019-09-02 14:03:17 +0100

    Initial commit.

---
+++ src/main/java/com/martinb/sbsocial/SbSocialApplication.java
@@ -0,0 +1,13 @@
+package com.martinb.sbsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSocialApplication.class, args);
+	}
+
+}
